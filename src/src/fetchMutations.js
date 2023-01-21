771e192a367f452194dedddbc164c0f2a8e8b2de
Alexander Gabriel <alex.barbalex@gmail.com>
2023-01-21 21:38:55 +0100

    limit fetch

--- src/src/fetchMutations.js
+++ src/src/fetchMutations.js
@@ -2,7 +2,9 @@ const fetchMutations = async ({ store }) => {
   const { setMutations, addError } = store
   let mutations = []
   try {
-    mutations = await window.electronAPI.query('SELECT * from mutations')
+    mutations = await window.electronAPI.query(
+      'SELECT * from mutations limit 1000',
+    )
   } catch (error) {
     addError(error)
   }
