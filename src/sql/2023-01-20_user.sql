041dff84e8726c5a69f77487d200eb20731e30b2
Alexander Gabriel <alex.barbalex@gmail.com>
2023-01-26 13:53:19 +0100

    edit import

--- src/sql/2023-01-20_user.sql
+++ src/sql/2023-01-20_user.sql
@@ -29,9 +29,6 @@ CREATE INDEX iUserIsAdmin ON users (isAdmin);
 -- TODO: need oliver's windows user name
 -- https://www.sqlite.org/lang_datefunc.html
 INSERT INTO users (name, isAdmin, letzteMutationZeit, letzteMutationUser)
-  VALUES ('admin', 1, 1674212203000.0, 'ag');
-
-INSERT INTO users (name, isAdmin, letzteMutationZeit, letzteMutationUser)
   VALUES ('alexa', 1, 1674212203000.0, 'ag');
 
 -- TODO: in pesonen letzeMutationZeit is 02.09.2019 for original import. Need to transform to unixepoch
