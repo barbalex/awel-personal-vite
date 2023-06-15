a6415df51b68c9939b2ac2785ec1476728e7e83b
Alexander Gabriel <alex.barbalex@gmail.com>
2023-06-15 12:28:02 +0200

    revert showing pwd on login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -56,11 +56,11 @@ const Login = () => {
   const onChange = useCallback((e) => setValue(e.target.value), [])
   const onBlur = useCallback(() => {
     if (!!value && value !== userPwd) {
-      // TODO: RESET
-      return setErrorMsg(
-        `Sie haben eingegeben: ${value}, das Passwort ist aber: ${userPwd}`,
-      )
-      // return setErrorMsg('Das Passwort ist falsch')
+      // This was to debug login problems
+      // return setErrorMsg(
+      //   `Sie haben eingegeben: ${value}, das Passwort ist aber: ${userPwd}`,
+      // )
+      return setErrorMsg('Das Passwort ist falsch')
     }
     if (!value) {
       return setErrorMsg('Bitte Passwort eingeben')
